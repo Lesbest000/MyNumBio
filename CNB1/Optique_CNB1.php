8d6bfcd2d790f3ac31e4be5b53de0f3114b97ccd
Eloidu59 <eloise.maurice@student.yncrea.fr>
2020-05-18 16:39:04 +0200

    créé toutes les pages épreuves

--- CNB1/Optique_CNB1.php
+++ CNB1/Optique_CNB1.php
@@ -3,18 +3,23 @@
     <head>
         <meta charset="utf-8" />
         <link rel="stylesheet" href="Style.css" />
-        <title>Page CNB1</title>
+        <title>Optique CNB1</title>
     </head>
 
     <body>
-        <h1>Epreuves CNB1</h1>
+        <h1>Optique CNB1</h1>
         
         <div class="contener_gauche">
             <div class="bloc_gauche"><a href="CNB1/Mathématiques_CNB1.php">Mathématiques</a></div>
             <div class="bloc_gauche"><a href="CNB1/Electronique_CNB1.php">Electronique</a></div>
             <div class="bloc_gauche"><a href="CNB1/Mécanique_CNB1.php">Mécanique</a></div>
-            <div class="bloc_gauche"><a href="CNB1/Optique_CNB1.php">Optique</a></div>
+            <div class="bloc_gauche_actuel"><a href="CNB1/Optique_CNB1.php">Optique</a></div>
             <div class="bloc_gauche"><a href="CNB1/Chimie_CNB1.php">Chimie</a></div>
+        </div>
+        <div class="contener_type_épreuve">
+            <div class="bloc_type_épreuve"><a href="Optique_CNB1/Quizz_Optique_CNB1.php">Quizz</a></div>
+            <div class="bloc_type_épreuve"><a href="Optique_CNB1/DS_Optique_CNB1.php">DS</a></div>
+            <div class="bloc_type_épreuve"><a href="Optique_CNB1/Partiel_Optique_CNB1.php">Partiel</a></div>
         </div>
     </body>
 </html>
