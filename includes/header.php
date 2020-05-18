13eeb3d1643fd833934f06ef9eaf8568a75bc31e
saaaniac <saniacalixte@gmail.com>
2020-05-19 00:54:44 +0200

    Update header.php

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>MyNumBio</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+  <header class="header-global">
+  <a href="Accueil.php"><img src="img/zovana3.png" class="logo" alt=""></a> <hr>
+    <nav id="barre-centrale">  
+      <a href="Accueil.php">Accueil</a>
+      <a href="cnb1.php">CNB1</a>
+      <a href="cnb2.php">CNB2</a>
+      <a href="cnb3.php">CNB3</a>
+    </nav>
+   </header>
