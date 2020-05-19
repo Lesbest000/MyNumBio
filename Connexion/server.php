2852ca2fe4914fb3092cff383f5332044c110bc2
Aya-Iguider <aya.iguider@student.yncrea.fr>
2020-05-19 14:39:43 +0200

    base de donée

--- Connexion/server.php
+++ Connexion/server.php
@@ -2,11 +2,18 @@
 $db=mysql_connect('localhost','root','','inscription');
 
 if($password_1 != $password_2){
-    array_push($error,"Les deux mots de passe sont différents")
+    array_push($error,"Les deux mots de passe sont différents");
 }
 
+if(isset($_POST['submit'])){
+    $email= mysql_real_escape_strings($_POST['email'])
+    $password= mysql_real_escape_strings($_POST['password']);
+}
 
-
-
+if(count($error)==0){
+    $password=md5($password_1);
+    $sql="INSERT INTO inscription (email, password) VALUES ('$email','$password')";
+    mysqli_query($db,$sql);
+}
 
 ?>
