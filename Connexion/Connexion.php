cfc3f5f62396de8163899c3cf0d62ef587a21e60
Manon000 <65402358+Manon000@users.noreply.github.com>
2020-05-19 13:08:08 +0200

    Update Connexion.php

--- Connexion/Connexion.php
+++ Connexion/Connexion.php
@@ -17,13 +17,13 @@
                 <button type="button" class="slide-btn"onclick="inscription()">Inscription</button>
             </div>
             <form id="connexion" class="input-group">
-            <input type="email" class="input-field" placeholder="Adresse mail" required>    
+            <input type="email" class="input-field" placeholder="Adresse mail" pattern="[A-Za-z0-9]{1,20}[.][A-Za-z0-9]{1,20}@student.yncrea.fr" title="Type d'adresse mail à fournir : prénom.nom@student.yncrea.fr" required>
             <input type="password" class="input-field" placeholder="Mot de passe" required>
             <input type="checkbox" class="checkbox"><span>Se souvenir de moi</span>
             <button type="submit" class="submit-btn">Se Connecter</button>
             </form>
             <form id="inscription" class="input-group">
-                    <input type="email" class="input-field" placeholder="Adresse mail" required>
+                    <input type="email" class="input-field" placeholder="Adresse mail" pattern="[A-Za-z0-9]{1,20}[.][A-Za-z0-9]{1,20}@student.yncrea.fr" title="Type d'adresse mail à fournir : prénom.nom@student.yncrea.fr" required>
                     <input type="password" class="input-field" placeholder="Mot de passe" required>
                     <input type="password" class="input-field" placeholder="Confirmer le mot de passe" required>
                     <p>&thinsp;</p>
