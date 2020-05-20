ea65c8fc011d9517cb62dd1778ae35a7b2205ed2
Aya-Iguider <aya.iguider@student.yncrea.fr>
2020-05-20 17:49:51 +0200

    JAI REUSSI

--- Connexion/server.php
+++ Connexion/server.php
@@ -1,7 +1,14 @@
 <?php
+
+
+//Va chercher valeurs dans le form
 $password_1=$_POST['password_1'];
 $password_2=$_POST['password_2'];
 
+$email = $_POST['email'];
+$password = $_POST['password_1'];
+$email = mb_strtolower($email);
+$password=md5($password_1);
 //Connexion a la bdd
 
 $db_host = "127.0.0.1";
@@ -18,48 +25,35 @@ if($link==false){
    echo"Erreur de connexion:".mysqli_connect_erno();
    die();
 }
-//
 
-//Integrer valeurs dans la bdd
-$requete = "SELECT * FROM `utilisateur` WHERE 1";
-
-$result=mysqli_query($link,$requete);
-
- if ( $result == FALSE )
- {
- echo "<p>Erreur d'exécution de la requete</p>" ;
- echo mysqli_errno($link) . ": " . mysqli_error($link). "\n";
- die();
- }
- else
- {
- echo "<p>SELECT a retourné " . mysqli_num_rows($result) . " lignes</p>" ;
- }
-
-if($row=mysqli_num_rows($result)>0){
-   echo"<ul>";
-   while($row=mysqli_fetch_assoc($result)){
-   echo"<li>".$row["email"]."</li>\n";
-   }  
-   echo"</ul>\n";
-}
 
 if($password_1!=$password_2){
    echo "Vos deux mots de passe ne sont pas les mêmes.</br>";
    die();
 }
 
-//Va chercher valeurs dans le form
-
-$email = $_POST['email'];
-$password = $_POST['password_1'];
-$email = mb_strtolower($email);
-$password=md5($password_1);
 
 
-;
+//Inserer dans bdd
+$sql_e = "SELECT * FROM `utilisateur` WHERE email='$email'";
+$res_e = mysqli_query($link,$sql_e);
 
-"INSERT INTO utilisateur(email, pass) VALUES ('$email','$password')"
+
+if(mysqli_num_rows($res_e) > 0){
+   echo "Cette adresse mail est utilisée"; 
+   die();	
+ }else{
+$sql="INSERT INTO `utilisateur`(`email`, `password`) VALUES('$email', '$password')";  
+$inscription=mysqli_query($link,$sql);
+
+if ( $inscription == FALSE )
+ {
+ echo "<p>Erreur d'inscription. Vous n'avez pas pu vous inscrire</p>" ;
+ echo mysqli_errno($link) . ": " . mysqli_error($link). "\n";
+ die();
+ }
+ }
+
 // Connection close  
 mysqli_close($link); 
 
