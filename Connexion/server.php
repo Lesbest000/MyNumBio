2b65128805589f75d3e2917db4ddcd3a941a7129
Aya-Iguider <aya.iguider@student.yncrea.fr>
2020-05-20 13:43:45 +0200

    Update server.php

--- Connexion/server.php
+++ Connexion/server.php
@@ -3,29 +3,39 @@ $password_1=$_POST['password_1'];
 $password_2=$_POST['password_2'];
 //bdd
 
-$db_host = 'devisenfdhmycnb.mysql.db';
-$db_user = 'devisenfdhmycnb';
-$db_pass = '1920MyNumBio';
-$db_name = 'devisenfdhmycnb';
-$link = mysqli_connect ($db_host,$db_user,$db_pass) or die ('Erreur : '.mysqli_error() );
-mysqli_select_db($db) or die ('Erreur :'.mysqli_error());
+$db_host = "127.0.0.1";
+$db_user = "root";
+$db_pass = "";
+$db_name = "test";
+$db_table="utilisateur";
+$link = mysqli_connect ($db_host,$db_user,$db_pass);
+mysqli_select_db($link,$db_table);
 
-$password_1=$_POST['password_1'];
-$password_2=$_POST['password_2'];
+if($link==false){
+   echo"Erreur de connexion:".mysqli_connect_erno();
+   die();
+}
 
-if($password_1==$password_2){
-   echo $password_2;
+if($password_1!=$password_2){
+   echo "Vos deux mots de passe ne sont pas les mêmes.</br>";
+   die();
 }
 $email = $_POST['email'];
 $password = $_POST['password_1'];
-$password=md5($password_1);
 $email = mb_strtolower($email);
+$password=md5($password_1);
 
-$sql="INSERT INTO utilisateur(email, password) VALUES ('$email','$password')";
-if($sql==false){
-   echo"fail";
-}else{
-   echo"Vous avez été inscrit";
-   header("");
-   }
+$requete="SELECT * from utilisateur where email='$email' && pass = '$password'" ;
+$result=mysqli_query($link,$requete);
+if($requete==false){
+   echo "Pb d'éxecution de la requete</br>";
+   echo mysqli_connect_erno($link)."\n";
+   die();
+}
+
+   $sql="INSERT into utilisateur(email , pass) values ('$email','$password')";
+   mysqli_query($link, $sql);
+   echo "c bon t'es co";
+mysqli_close($link);
+
 ?>
