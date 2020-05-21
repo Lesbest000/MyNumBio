53d2e9dbd125033ba32c8f2d612b82d7d9a39589
Manon000 <65402358+Manon000@users.noreply.github.com>
2020-05-21 14:47:31 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -6,10 +6,14 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
     <link rel="stylesheet" href="Style.css">
   </head>
+
   <body>
+
   <header class="header-global">
+  
   <a href="Accueil.php"><img src="img/zovana3.png" class="logo" alt=""></a> <hr>
-    <nav id="barre-centrale">  
+    <nav id="barre-centrale"> 
+
       <a href="Accueil.php">ACCUEIL</a>
       <a href="Page_CNB1.php">CNB1</a>
       <a href="Page_CNB2.php">CNB2</a>
