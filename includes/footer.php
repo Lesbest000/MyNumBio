de0440c602303b6fd1e320e2d9b02bef43e8bf40
saaaniac <saniacalixte@gmail.com>
2020-05-25 00:20:47 +0200

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,9 @@
 <br>
 <div class=top>  
 <footer>
-<a href="https://www.facebook.com/ISEN.Lille/"><i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i>  </a>
-<a href="https://twitter.com/isenlille?lang=fr"><i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i>  </a>
-<a href="https://www.instagram.com/isen.lille/?hl=fr"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i>  </a>
+<a href="https://www.facebook.com/ISEN.Lille/"><i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i></a>
+<a href="https://twitter.com/isenlille?lang=fr"><i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i></a>
+<a href="https://www.instagram.com/isen.lille/?hl=fr"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
 <a href="https://www.youtube.com/user/ISENLille/"><i class="fa fa-youtube-square fa-2x" aria-hidden="true"></i></a>
    <br> <a href="#top">Haut de page</a>
    <br> <h4>Site créé par des CNB pour les CNB</h4>
